758b0e7451b73957c8b3bfc2f2fdc0a06de5958d
streakk <laurynas.andrijevskis@yahoo.com>
2023-10-19 10:37:42 +0300

    Create post.lua

---
+++ post.lua
@@ -0,0 +1,3 @@
+wrk.method = "POST"
+wrk.body   = '{"values": [1,2,3,4,5,6,7,8,9,10,11,12,13,123,312312,314,123,123,123,4,123,15,12,315,12,51,23,123123,12,31]}'
+wrk.headers["Content-Type"] = "application/json"
